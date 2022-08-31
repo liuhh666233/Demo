6084f939f20fe0c55e617bfe475fa3cd728d79e4
lxb <liuxiaobo666233@gmail.com>
2022-09-01 07:45:57 +0800

    Format code.

--- code/ecf/snooze.c
+++ code/ecf/snooze.c
@@ -1,5 +1,26 @@
-unsigned int snooze(unsigned int secs){
-    unsigned int  rc = sleep(secs);
-    printf("Slept for %d of %d s. \n", secs - rc, rc);
+#include "csapp.h"
+// SIGINT handler
+void handler(int sig)
+{
+    return;
+}
+
+unsigned int snooze(unsigned int secs)
+{
+    unsigned int rc = sleep(secs);
+    printf("Slept for %d of %d secs.\n", secs - rc, secs);
     return rc;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage:%s <secs>\n", argv[0]);
+        exit(0);
+    }
+    if (signal(SIGINT, handler) == SIG_ERR)
+        unix_error("signal error.\n");
+    (void)snooze(atoi(argv[1]));
+    exit(0);
 }
