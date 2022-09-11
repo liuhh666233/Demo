50ebe18415ddbe47d8a89c391dce9ce4981a6423
lxb <liuxiaobo666233@gmail.com>
2022-09-11 15:36:29 +0800

    Tests

--- code/ecf/procmask1.c
+++ code/ecf/procmask1.c
@@ -17,11 +17,11 @@ void handler(int sig)
 {
     pid_t pid;
     while ((pid = waitpid(-1, NULL, 0)) > 0) /* Reap a zombie child */
-	deletejob(pid); /* Delete the child from the job list */
+        deletejob(pid);                      /* Delete the child from the job list */
     if (errno != ECHILD)
-	unix_error("waitpid error");
+        unix_error("waitpid error");
 }
-    
+
 int main(int argc, char **argv)
 {
     int pid;
@@ -29,14 +29,16 @@ int main(int argc, char **argv)
     Signal(SIGCHLD, handler);
     initjobs(); /* Initialize the job list */
 
-    while (1) {
-	/* Child process */
-	if ((pid = Fork()) == 0) {
-	    Execve("/bin/date", argv, NULL);
-	}
+    while (1)
+    {
+        /* Child process */
+        if ((pid = Fork()) == 0)
+        {
+            Execve("/bin/date", argv, NULL);
+        }
 
-	/* Parent process */
-	addjob(pid);  /* Add the child to the job list */
+        /* Parent process */
+        addjob(pid); /* Add the child to the job list */
     }
     exit(0);
 }
